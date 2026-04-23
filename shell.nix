6e92c0e46a7d29ba1e442692fbcad39529b1838e
xe46wam <Lukas.Lamprecht@uni-jena.de>
2026-04-23 11:57:41 +0200

    nix-shell vollständig

--- shell.nix
+++ shell.nix
@@ -6,6 +6,8 @@ pkgs.mkShell {
             python-pkgs.matplotlib
             python-pkgs.sphinx
             python-pkgs.sphinx_rtd_theme
+            python-pkgs.pandas
+            python-pkgs.imageio
         ]))
         pkgs.doxygen
         pkgs.gmt
