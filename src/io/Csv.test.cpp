a2546cead538b9d7fed4ef17c1ed0f1db38fd26d
KonardNeihn <jahn.konrad@web.de>
2026-04-29 00:17:22 +0200

    adjustet tests

--- src/io/Csv.test.cpp
+++ src/io/Csv.test.cpp
@@ -28,12 +28,12 @@ TEST_CASE( "Test the CSV-writer for 1D settings.", "[CsvWrite1d]" ) {
                                nullptr,
                                l_stream0 );
 
-  std::string l_ref0 = R"V0G0N(x,y,height,momentum_x
-0.25,0.25,1,5
-0.75,0.25,2,4
-1.25,0.25,3,3
-1.75,0.25,4,2
-2.25,0.25,5,1
+  std::string l_ref0 = R"V0G0N(x,y,bathymetry,height,momentum_x
+0.25,0.25,0,1,5
+0.75,0.25,0,2,4
+1.25,0.25,0,3,3
+1.75,0.25,0,4,2
+2.25,0.25,0,5,1
 )V0G0N";
 
   REQUIRE( l_stream0.str().size() == l_ref0.size() );
@@ -66,11 +66,11 @@ TEST_CASE( "Test the CSV-writer for 2D settings.", "[CsvWrite2d]" ) {
                                l_hv+4+1,
                                l_stream1 );
 
-  std::string l_ref1 = R"V0G0N(x,y,height,momentum_x,momentum_y
-5,5,5,10,5
-15,5,6,9,9
-5,15,9,6,6
-15,15,10,5,10
+  std::string l_ref1 = R"V0G0N(x,y,bathymetry,height,momentum_x,momentum_y
+5,5,0,5,10,5
+15,5,0,6,9,9
+5,15,0,9,6,6
+15,15,0,10,5,10
 )V0G0N";
 
   REQUIRE( l_stream1.str().size() == l_ref1.size() );
