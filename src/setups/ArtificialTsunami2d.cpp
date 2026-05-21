2fab6783ef80d88512372f7319143ca9e1233e69
xe46wam <Lukas.Lamprecht@uni-jena.de>
2026-05-21 18:40:12 +0200

    Fix Berechnung

--- src/setups/ArtificialTsunami2d.cpp
+++ src/setups/ArtificialTsunami2d.cpp
@@ -17,15 +17,15 @@ tsunami_lab::setups::ArtificialTsunami2d::ArtificialTsunami2d(t_real i_bIn,
     m_roh = i_roh;
 }
 
-tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getHeight( t_real i_x, t_real i_y ) const {
+tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getHeight( t_real , t_real ) const {
 
-    t_real l_f = std::sin((i_x / 500) + 1) * 3.141592653589793;
-    t_real l_g = -((i_y / 500)*(i_y / 500)) + 1;
+    //t_real l_f = std::sin(((i_x / 500) + 1) * 3.141592653589793);
+    //t_real l_g = -((i_y / 500)*(i_y / 500)) + 1;
     
-    t_real l_d = 5 * l_f * l_g;
+    //t_real l_d = 5 * l_f * l_g;
 
     if (m_bIn < 0) {
-        std::cout << "Height: " << -std::min(m_bIn, -m_roh) + l_d << std::endl;
+        //std::cout << "Height: " << -std::min(m_bIn, -m_roh) + l_d << std::endl;
         return std::max(-m_bIn, m_roh);
     } else {
         return 0;
@@ -46,7 +46,7 @@ tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getBathymetry( t_r
                                                                             t_real i_y ) const {
     (void)i_x;
     (void)i_y;
-    t_real l_f = std::sin((i_x / 500) + 1) * 3.141592653589793;
+    t_real l_f = std::sin(((i_x / 500) + 1) * 3.141592653589793);
     t_real l_g = -((i_y / 500)*(i_y / 500)) + 1;
     
     t_real l_d = 5 * l_f * l_g;
