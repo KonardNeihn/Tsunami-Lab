e90e0ba57bb13b2d979da6a537e95498c647ab25
KonardNeihn <jahn.konrad@web.de>
2026-06-01 12:21:43 +0200

    fixing merge

--- src/main.cpp
+++ src/main.cpp
@@ -39,21 +39,9 @@ int is_number(char* input);
 
 int main( int   i_argc,
           char *i_argv[] ) {
-  // number of cells in x- and y-direction
-  tsunami_lab::t_idx l_nx = 100;
-  tsunami_lab::t_idx l_ny = 100;
-
-  // width of the observed space
-  tsunami_lab::t_real l_w = 10.0;
-
-  // time until abortion
-  tsunami_lab::t_real l_endTime = 1.25;
 
   // current simulation time
   tsunami_lab::t_real l_simTime = 0;
-
-  // set cell size
-  tsunami_lab::t_real l_dxy = l_w / l_nx;
 
   // Domainstart
   tsunami_lab::t_real l_domainStartX = 0.0;
@@ -61,15 +49,6 @@ int main( int   i_argc,
 
   // rate at which checkpoints are written (relative to end time)
   tsunami_lab::t_real l_checkpointRate = 0.5; 
-
-  // string of selected solver
-  std::string l_solver = "fwave";
-
-  // possible insanity
-  std::string l_sanity = "true";
-
-  // string of selected setup
-  std::string l_setup_selection = "DamBreak1d";
 
   std::cout << "####################################" << std::endl;
   std::cout << "### Tsunami Lab                  ###" << std::endl;
@@ -80,123 +59,6 @@ int main( int   i_argc,
   // Handling input options
   Config l_config = parseArgs(i_argc, i_argv);
 
-  for (int i = 1; i < i_argc; i++) {
-    std::string arg = i_argv[i];
-
-    // check if help is needed
-    if (arg == "-h" || arg == "--help") {
-      std::cout << "__________Manual__________" << std::endl;
-      std::cout << std::endl;
-      std::cout << "Usage: ./build/tsunami_lab <N_CELLS_X> [OPTIONS]" << std::endl;
-      std::cerr << "where N_CELLS_X is the number of cells in x-direction" << std::endl;
-      std::cout << std::endl;
-      std::cout << "  Option       Long option         Meaning" << std::endl;
-      std::cout << "  -h,          --help              Display this help and exit." << std::endl;
-      std::cout << "  -s <name>,   (--solver=<name>)   Select a solver. Possible is {fwave, roe}. Default is fwave." << std::endl;
-      std::cout << "  -i,          --insanity          Set flag for insanity. Default is sanity" << std::endl; // oder --sanity=true  ?
-      std::cout << "  -n <number>, (--ncells=<number>) Set number of cells. Default is 100." << std::endl;
-      std::cout << "  -n <number> <number>             Set number of cells in x and y direction. Defaults: x = 100, y = 1" << std::endl;
-      std::cout << "  -w <number>, (--width=<number>)  Set width of the observed space in meters. default is 10" << std::endl;
-      std::cout << "  -t <number>, (--time=<number>)   Set time until aborting in s. default is 1.25" << std::endl;
-      std::cout << "  -S <name>,   (--setup=<name>)    Select setup to simulate. Possible is {DamBreak1d, RareRare1d, ShockShock1d, Bathymetry1d, SubcriticalFlow1d, HydraulicJump1d, TsunamiEvent1d, CircularDamBreak2d, DamBreak2d, ArtificialTsunami2d, TsunamiEvent2d, ChileEvent2d, TohokuEvent2d}. Default is DamBreak1d" << std::endl;
-      return EXIT_SUCCESS;
-    }
-    // check if N_CELLS_X is passed
-    else if (is_number(i_argv[i])) {
-      l_nx = atoi( i_argv[i] );
-      if( l_nx < 1 ) {
-        std::cerr << "invalid number of cells, must be > 0" << std::endl;
-        return EXIT_FAILURE;
-      }
-      l_dxy = l_w / l_nx;
-    }
-    // ckeck if a solver is specified
-    else if (arg == "-s" && i + 1 < i_argc) {  // || arg == "--solver=<>"
-      l_solver = i_argv[++i]; // nächstes argument lesen
-
-      if (l_solver != "fwave" && l_solver != "roe") {
-        std::cout << "tsunami_lab: invalid solver '" << i_argv[i] << "'" << std::endl;
-        std::cout << "Try 'tsunami_lab --help' for more information." << std::endl;
-        return EXIT_FAILURE;
-      }
-    }
-    // ckeck if number of cells is specified only in x direction
-    else if (arg == "-n" && i + 1 < i_argc) {  // || arg == "--ncells=<>"
-      if (is_number(i_argv[++i])) {
-        l_nx = atoi( i_argv[i] );
-        if( l_nx < 1 ) {
-          std::cerr << "invalid number of cells, must be > 0" << std::endl;
-          return EXIT_FAILURE;
-        }
-        l_dxy = l_w / l_nx;
-      }
-    }
-    // check if number of cells is specified for x and y direction
-    else if (arg == "-n" && i + 2 < i_argc) {  // || arg == "--ncells=<>"
-      if (is_number(i_argv[++i]) && is_number(i_argv[++i])) {
-        l_nx = atoi(i_argv[i - 1]);
-        l_ny = atoi(i_argv[i]);
-        if(l_nx < 1 || l_ny < 1) {
-          std::cerr << "invalid number of cells, must be > 0" << std::endl;
-          return EXIT_FAILURE;
-        }
-        l_dxy = l_w / l_nx;
-      }
-    }
-    // ckeck if observed width is specified
-    else if (arg == "-w" && i + 1 < i_argc) {  // || arg == "--width=<>"
-      if (is_number(i_argv[++i])) {
-        l_w = atoi( i_argv[i] );
-        if( l_w <= 0 ) {
-          std::cerr << "invalid width, must be > 0" << std::endl;
-          return EXIT_FAILURE;
-        }
-        l_dxy = l_w / l_nx;
-      }
-    }
-    // ckeck if max time is specified
-    else if (arg == "-t" && i + 1 < i_argc) {  // || arg == "--time=<>"
-      if (is_number(i_argv[++i])) {
-        l_endTime = atoi( i_argv[i] );
-        if( l_endTime <= 0 ) {
-          std::cerr << "invalid time, must be > 0" << std::endl;
-          return EXIT_FAILURE;
-        }
-      }
-    }
-    // ckeck if a setup is specified
-    else if (arg == "-S" && i + 1 < i_argc) {  // || arg == "--solver=<>"
-      l_setup_selection = i_argv[++i]; // nächstes argument lesen
-      if (l_setup_selection != "DamBreak1d" && l_setup_selection != "RareRare1d" 
-                                            && l_setup_selection != "ShockShock1d"  
-                                            && l_setup_selection != "Bathymetry1d"
-                                            && l_setup_selection != "SubcriticalFlow1d"
-                                            && l_setup_selection != "HydraulicJump1d"
-                                            && l_setup_selection != "TsunamiEvent1d"
-                                            && l_setup_selection != "CircularDamBreak2d"
-                                            && l_setup_selection != "DamBreak2d"
-                                            && l_setup_selection != "ArtificialTsunami2d"
-                                            && l_setup_selection != "TsunamiEvent2d"
-                                            && l_setup_selection != "ChileEvent2d"
-                                            && l_setup_selection != "TohokuEvent2d"
-                                            && l_setup_selection != "Checkpoint2d"
-                                          ) {
-        std::cout << "tsunami_lab: invalid setup '" << i_argv[i] << "'" << std::endl;
-        std::cout << "Try 'tsunami_lab --help' for more information." << std::endl;
-        return EXIT_FAILURE;
-      }
-    }
-    // check if insanity is wanted
-    else if (arg == "-i" || arg == "--insanity") {
-      l_sanity = "false";
-    }
-    // handling unknown argument
-    else {
-      std::cout << "tsunami_lab: invalid option '" << i_argv[i] << "'" << std::endl;
-      std::cout << "Try 'tsunami_lab --help' for more information." << std::endl;
-      return EXIT_FAILURE;
-    }
-  }
   // construct setup
   auto l_setup = createSetup(l_config);
 
@@ -205,7 +67,7 @@ int main( int   i_argc,
   std::cout << "  number of cells in x-direction: " << l_config.nx << std::endl;
   std::cout << "  number of cells in y-direction: " << l_config.ny << std::endl;
   std::cout << "  cell size:                      " << l_config.dxy << std::endl;
-  std::cout << "  sanity:                         " << l_config.insanity << std::endl;
+  std::cout << "  insanity:                       " << l_config.insanity << std::endl;
   std::cout << "  selected solver:                " << l_config.solver << std::endl;
   std::cout << "  selected time:                  " << l_config.endTime << std::endl;
 
@@ -233,134 +95,6 @@ int main( int   i_argc,
   // set output path for NetCdf
   std::string l_ncPath = "solutions/netcdf_output.nc";   
 
-  // construct setup
-  tsunami_lab::setups::Setup *l_setup;
-  tsunami_lab::setups::TsunamiEvent1d *l_tsunami = nullptr; 
-  // set if simulation is 2D as to keep 1D-simulations working
-  bool l_is2D = false;
-
-  if (l_sanity == "true"){
-    if (l_setup_selection == "DamBreak1d") { // könnte man auch gleich oben in der argumentübergabe machen?
-      l_setup = new tsunami_lab::setups::DamBreak1d( 10,    // height left
-                                                      5,    // height right
-                                                      5 );  // dam location
-    } else if (l_setup_selection == "RareRare1d") {
-      l_setup = new tsunami_lab::setups::RareRare1d( 10,    // height 
-                                                     10,    // momentum
-                                                     (l_w / 2) );   // middle location
-    } else if (l_setup_selection == "ShockShock1d") {
-      l_setup = new tsunami_lab::setups::ShockShock1d( 10,    // height
-                                                       10,    // momentum
-                                                        5 );  // impact location
-    } else if (l_setup_selection == "Bathymetry1d") {
-      l_setup = new tsunami_lab::setups::Bathymetry1d( 12,          // height left
-                                                       7,          // height right
-                                                       (l_w / 3),   // dam location
-                                                       0,           // bathymetry left
-                                                       5,           // bathymetry right
-                                                       ((2 * l_w) / 3) );   // bathymetry change location
-    } else if (l_setup_selection == "SubcriticalFlow1d") {
-      l_setup = new tsunami_lab::setups::SubcriticalFlow1d( l_w / 2,   // bump location in the middle of the simulation
-                                                            0.8,       // bump height: 0.8m 
-                                                            0.5,       // bump width: 0.5m
-                                                            2.0,       // water surface at 2m, well above the 0.8m bump
-                                                            1.5        // constant momentum (positive means left to right)
-                                                          );
-    } else if (l_setup_selection == "HydraulicJump1d") {
-      l_setup = new tsunami_lab::setups::HydraulicJump1d( -0.13, // obstacle height
-                                                          -0.33, // base height of the bathymetry
-                                                          0.18  // momentum of the water
-                                                        );
-    } else if (l_setup_selection == "TsunamiEvent1d") {
-      l_tsunami = new tsunami_lab::setups::TsunamiEvent1d( "src/bathymetry/output/03_dem_03.csv"
-                                                          );
-      l_setup = l_tsunami;
-      
-      // Use actual domain width from the CSV x-coordinates
-      l_w   = l_tsunami->getDomainWidth();
-      l_dxy = l_w / l_nx;
-
-    } else if (l_setup_selection == "CircularDamBreak2d") {
-      l_w   = 100.0;
-      l_ny  = l_nx;
-      l_dxy = l_w / l_nx;
-      l_setup = new tsunami_lab::setups::CircularDamBreak2d();
-
-    } else if (l_setup_selection == "DamBreak2d") {
-      l_ny  = l_nx;
-      l_setup = new tsunami_lab::setups::DamBreak2d(15, // height water dam
-                                                    10, // height water
-                                                    2.5, // position of dam X
-                                                    5,  // position of dam Y
-                                                    2.5); // radius of dam
-    } else if (l_setup_selection == "ArtificialTsunami2d") {
-      l_ny = l_nx;
-      l_w = 1000;
-      l_dxy = l_w / l_nx;
-      l_setup = new tsunami_lab::setups::ArtificialTsunami2d(-100.0, 
-                                                            0);
-    } else if (l_setup_selection == "TsunamiEvent2d") {
-      l_ny = l_nx;
-      l_w   = 10.0;
-      l_dxy = l_w / l_nx;
-      l_setup = new tsunami_lab::setups::TsunamiEvent2d("src/NetCdf/artificialtsunami_bathymetry_1000.nc",
-                                                        "src/NetCdf/artificialtsunami_displ_1000.nc");
-    } else if (l_setup_selection == "ChileEvent2d") {
-      l_nx = 100;
-      l_ny = l_nx;
-      l_w   = l_nx;
-      l_dxy = l_w / l_nx;
-      l_setup = new tsunami_lab::setups::ChileEvent2d("src/bathymetry/output/chile_gebco20_usgs_250m_bath_fixed.nc",
-                                                        "src/bathymetry/output/chile_gebco20_usgs_250m_displ_fixed.nc",
-                                                      l_nx, l_ny);
-
-    } else if (l_setup_selection == "TohokuEvent2d") {
-      l_nx = 1000;
-      l_ny = l_nx;
-      l_w   = l_nx;
-      l_dxy = l_w / l_nx;
-      l_setup = new tsunami_lab::setups::TohokuEvent2d("src/bathymetry/output/tohoku_gebco20_usgs_250m_bath.nc",
-                                                        "src/bathymetry/output/tohoku_gebco20_usgs_250m_displ.nc",
-                                                      l_nx, l_ny);
-     } else if (l_setup_selection == "Checkpoint2d") {
-        auto l_checkpointSetup = new tsunami_lab::setups::Checkpoint2d("solutions/checkpoint.nc");
-        l_nx = l_checkpointSetup->getNX();
-        l_ny = l_checkpointSetup->getNY();
-        l_w = l_checkpointSetup->getWidth();
-        l_dxy = l_w / l_nx;
-        l_domainStartX = l_checkpointSetup->getDomainStartX();
-        l_domainStartY = l_checkpointSetup->getDomainStartY();
-        l_simTime = l_checkpointSetup->getLastTimeStep();
-        l_endTime = l_checkpointSetup->getEndTime();
-        l_setup = l_checkpointSetup;
-     }
-    
-    else {
-      std::cerr << "Somethings wrong. Did you add the setup_selection?" << std::endl;
-      return EXIT_FAILURE;
-    }
-  }
-  // very rudimentary sanity-check, to be expanded later
-  else if (l_sanity == "false") {
-  l_setup = new tsunami_lab::setups::ShockShock1d( 8899.326826472694,
-                                                   122.0337839252433,
-                                                   5 );
-  } else {
-        std::cerr << "Still sane?" << std::endl;
-        return EXIT_FAILURE;
-      }
-
-
-  // check if setup is 2d ("2d" HAS TO BE INCLUDED IN SETUP NAME)
-  l_is2D = false;
-  int l_stringLength = l_setup_selection.length();
-  for (int i = 1; i < l_stringLength; i++) {
-    if (l_setup_selection[i-1] == '2' && l_setup_selection[i] == 'd') {
-      l_is2D = true;
-      break;
-    }
-  }
-
  // construct solver
  tsunami_lab::patches::WavePropagation *l_waveProp;
   if( l_config.is_2d ) {
@@ -374,12 +108,6 @@ int main( int   i_argc,
 
   // set up solver
   for( tsunami_lab::t_idx l_cy = 0; l_cy < l_config.ny; l_cy++ ) {
-
-    for( tsunami_lab::t_idx l_cx = 0; l_cx < l_nx; l_cx++ ) {
-        tsunami_lab::t_real l_x = (l_cx + 0.5) * l_dxy + l_domainStartX;
-        tsunami_lab::t_real l_y = l_is2D ? (l_cy + 0.5) * l_dxy + l_domainStartY : l_cy * l_dxy;
-    //tsunami_lab::t_real l_domainStartX = 0.0;
-    //tsunami_lab::t_real l_domainStartY = 0.0;
 
     for( tsunami_lab::t_idx l_cx = 0; l_cx < l_config.nx; l_cx++ ) {
 
@@ -436,7 +164,6 @@ int main( int   i_argc,
   // set up time and print control
   tsunami_lab::t_idx  l_timeStep = 0;
   tsunami_lab::t_idx  l_nOut = 0;
-  //tsunami_lab::t_real l_endTime = 1.25; is set above
 
   std::cout << "entering time loop" << std::endl;
 
@@ -476,31 +203,6 @@ tsunami_lab::t_real l_checkpointTimer = 0.0;
       if( l_timeStep % 25 == 0 ) {
           std::cout << "  simulation time: " << l_simTime << " time steps: " << l_timeStep << std::endl;
 
-          //std::string l_path = (outDir / ("solution_" + std::to_string(l_nOut) + ".csv")).string();
-          //std::cout << "  writing wave field to " << l_path << std::endl;
-        /*
-          std::ofstream l_file;
-          l_file.open( l_path );
-
-        
-          if( l_is2D ) {
-              tsunami_lab::io::Csv::write( l_dxy, l_nx, l_ny,
-                                          l_waveProp->getStride(),
-                                          l_waveProp->getBathymetry(),
-                                          l_waveProp->getHeight(),
-                                          l_waveProp->getMomentumX(),
-                                          l_waveProp->getMomentumY(),
-                                          l_file );
-          } else {
-              tsunami_lab::io::Csv::write( l_dxy, l_nx, 1, 1,
-                                          l_waveProp->getBathymetry(),
-                                          l_waveProp->getHeight(),
-                                          l_waveProp->getMomentumX(),
-                                          nullptr,
-                                          l_file );
-          }
-          l_file.close();
-      */
           std::string l_path = (outDir / ("solution_" + std::to_string(l_nOut) + ".csv")).string();
           std::cout << "  writing wave field to " << l_path << std::endl;
 
@@ -548,38 +250,38 @@ tsunami_lab::t_real l_checkpointTimer = 0.0;
 
       std::string checkpointPath = "solutions/checkpoint.nc";
       std::cout << "Creating checkpoint to " << checkpointPath << std::endl;
-      l_checkpoint.createCheckpoint(checkpointPath, l_nx, l_ny, l_simTime, l_endTime, l_w, l_domainStartX, l_domainStartY);
+      l_checkpoint.createCheckpoint(checkpointPath, l_config.nx, l_config.ny, l_simTime, l_config.endTime, l_config.width, l_domainStartX, l_domainStartY);
       
       l_checkpoint.write2DVariable(
         checkpointPath,
         "height",
         l_waveProp->getHeight(),
-        l_nx,
-        l_ny
+        l_config.nx,
+        l_config.ny
       );
 
       l_checkpoint.write2DVariable(
           checkpointPath,
           "bathymetry",
           l_waveProp->getBathymetry(),
-          l_nx,
-          l_ny
+          l_config.nx,
+          l_config.ny
       );
 
       l_checkpoint.write2DVariable(
           checkpointPath,
           "momentumX",
           l_waveProp->getMomentumX(),
-          l_nx,
-          l_ny
+          l_config.nx,
+          l_config.ny
       );
 
       l_checkpoint.write2DVariable(
           checkpointPath,
           "momentumY",
           l_waveProp->getMomentumY(),
-          l_nx,
-          l_ny
+          l_config.nx,
+          l_config.ny
       );
 
       std::cout << "Finished creating Checkpoint" << std::endl;
@@ -600,18 +302,4 @@ tsunami_lab::t_real l_checkpointTimer = 0.0;
 
   std::cout << "finished, exiting" << std::endl;
   return EXIT_SUCCESS;
-}
-
-int is_number(char* input) {
-  char* end;
-  errno = 0;
-
-  //long val = std::strtol(input, &end, 10);
-  std::strtol(input, &end, 10);
-
-  // Validierung
-  if (end == input || *end != '\0' || errno != 0) {
-    return 0;
-  }
-  return 1;
 }
